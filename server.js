89d2c689548673e29e21cf7bda0c24d2e04d903f
David Kopp <David_Kopp@compaid.com>
2016-09-12 07:30:24 -0400

    update middleware for localhost

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 const ENV = app.settings.env;
-const DEV_ENV = "development";
 
 app.use(function(req, res, next) {
-    if (ENV !== DEV_ENV) {
-        if (req.headers['x-forwarded-proto'] === 'http') {
-            next();
-        } else {
-            res.redirect('http://' + req.hostname + req.url);
-        }
-    }
+  if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+  } else {
+      next();
+  }
 });
 
 app.use(express.static('public'));
